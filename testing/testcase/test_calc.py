5daf43ce553e9d114e043bc2fcf701195ade7b10
“zhangmin <531474715@qq.com>
2020-05-11 23:59:06 +0800

    测试代码提交

--- testing/testcase/test_calc.py
+++ testing/testcase/test_calc.py
@@ -9,15 +9,30 @@ from python.calc import Calc
 import pytest
 import yaml
 
+
 class TestCalc:
     def setup(self):
         self.calc = Calc()
 
-    @pytest.mark.parametrize("data1,data2,expect",yaml.safe_load(open('../datas/add.yaml')))
-    def calc_add(self,data1,data2,expect):
-        result = self.calc.add(data1,data2)
+    @pytest.mark.parametrize('num1,num2,expect', yaml.safe_load(open('../datas/add.yml')))
+    def calc_add(self, num1, num2, expect):
+        result = self.calc.add(num1, num2)
         assert expect == result
 
+    @pytest.mark.parametrize('num1,num2,expect',yaml.safe_load(open('../datas/sub.yml')))
+    def calc_sub(self, num1, num2, expect):
+        result = self.calc.sub(num1, num2)
+        assert expect == result
+
+    @pytest.mark.parametrize('num1,num2,expect', yaml.safe_load(open('../datas/mul.yml')))
+    def calc_mul(self, num1, num2, expect):
+        result = self.calc.mul(num1, num2)
+        assert expect == result
+
+    @pytest.mark.parametrize('num1,num2,expect', yaml.safe_load(open('../datas/div.yml')))
+    def calc_div(self, num1, num2, expect):
+        result = self.calc.div(num1, num2)
+        assert expect == result
 
 if __name__ == '__main__':
     pytest.main()
