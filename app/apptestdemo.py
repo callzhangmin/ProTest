17b37c6e5360a0f227dfd5f69b9ad6536120202a
“zhangmin <531474715@qq.com>
2020-05-27 23:58:16 +0800

    appium企业微信

--- app/apptestdemo.py
+++ app/apptestdemo.py
@@ -3,6 +3,8 @@
 # @Time : 2020/5/19 23:42
 # @Author : zhangmin
 # @File : apptestdemo.py
+from time import sleep
+
 import pytest
 from appium import webdriver
 from appium.webdriver.common.mobileby import MobileBy
@@ -17,20 +19,24 @@ class TestXueQiu:
         self.desired_caps['appPackage'] = 'com.xueqiu.android'  # 雪球app的包名
         self.desired_caps['appActivity'] = '.view.WelcomeActivityAlias'  # app 的主 activity（启动的第一个页面）
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', self.desired_caps)  # 连通过 ip 和端口 连接模拟器
-        self.desired_caps['noReset'] = "true"
-        self.desired_caps['dontStopAppOnReset'] = 'true'
+        self.desired_caps['noReset'] = True
+        self.desired_caps["autoGrantPermissions"]= True
+        # self.desired_caps['dontStopAppOnReset'] = 'true'
+        self.desired_caps["skipDeviceInitialization"] = True
         self.driver.implicitly_wait(10)
 
     def teardown(self):
         self.driver.quit()
 
-    @pytest.mark.parametrize('searchkey,text,type', [
-        ('alibaba', '阿里巴巴', 'BABA'),
-        ('xiaomi', '小米集团-W', '01810')
+    @pytest.mark.parametrize('searchkey,text', [
+        ('alibaba', '阿里巴巴'),
+        ('xiaomi', '小米集团-W')
     ])
-    def test_search(self,searchkey,text,type):
+    def test_search(self,searchkey,text):
         try:
-            self.driver.find_element(MobileBy.ID,'com.xueqiu.android:id/tv_search').click()
-            self.driver.find_element(MobileBy.ID,'com.xueqiu.android:id/search_input_text').send_keys(searchkey)
+            self.driver.find_element(MobileBy.ID,'home_search').click()
+            self.driver.find_element(MobileBy.ID,'search_input_text').send_keys(searchkey)
+            self.driver.find_element(MobileBy.ID,'id/name').click()
+            sleep(5)
         except EnvironmentError as e:
             print(e)
--- app/test_search.py
+++ app/test_search.py
@@ -3,7 +3,10 @@
 # @Time : 2020/5/24 15:09
 # @Author : zhangmin
 # @File : test_search.py
+from time import sleep
+
 from appium import webdriver
+from appium.webdriver.common.mobileby import MobileBy
 
 
 class TestWeiXin:
@@ -13,12 +16,38 @@ class TestWeiXin:
         self.desired_caps['platformVersion'] = '6.0.1'
         self.desired_caps['deviceName'] = '127.0.0.1:7555'
         self.desired_caps['appPackage'] = 'com.tencent.wework'
+        self.desired_caps['noReset'] = "true"
         self.desired_caps['appActivity'] = '.launch.LaunchSplashActivity'
+        self.desired_caps['skipServerInstallation'] = True
+        self.desired_caps['skipDeviceInitialization'] = True
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', self.desired_caps)
         self.driver.implicitly_wait(10)
 
     def teardown(self):
-        self.driver = quit()
+        self.driver.quit()
 
-    def test_add(self):
-        pass
+    def test_addcontact(self):
+        print("添加联系人")
+        self.driver.find_element(MobileBy.XPATH, "//*android.widget.TextView[@text='通讯录']").click()
+        self.driver.find_element(MobileBy.XPATH, "//*android.widget.TextView[@text='添加成员']").click()
+        self.driver.find_element(MobileBy.XPATH, "//*android.widget.TextView[@text='手动输入添加']").click()
+        self.driver.find_element(MobileBy.XPATH, "//*android.widget.TextView[@text='姓名　']/..//*[@class='android.widget.EditText']").send_keys(
+            "张敏")
+        self.driver.find_element(MobileBy.XPATH,
+                                 "//*[@text='性别']/..//*[contains(@class, 'TextView') and @text='男']").click()
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='女']").click()
+        self.driver.find_element(MobileBy.XPATH,
+                                 "//*[@text='手机　']/..//*[contains(@class, 'EditText')]").send_keys("15397399245")
+        self.driver.find_element(MobileBy.XPATH,
+                                 "//*[@text='邮箱　']/..//*[contains(@class, 'EditText')]").send_keys("531474715@163.com")
+        self.driver.find_element(MobileBy.ID, "com.tencent.wework:id/gvk").click()
+        sleep(1)
+        self.driver.find_element(MobileBy.XPATH, "//*[@text='添加成功']")
+    def test_delcontact(self):
+        print('删除联系人')
+        self.driver.find_element(MobileBy.XPATH,"//*[@text='通讯录']").click()
+        els2 = driver.find_elements_by_id("drb")
+        els3 = driver.find_elements_by_id("drb[1]")
+        els4 = driver.find_elements_by_xpath("//*android.widget.TextView[@text='通讯录']")
+
+
